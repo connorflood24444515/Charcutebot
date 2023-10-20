428dda2e068dc4ca8557c6aa4e66301b2f4db650
SamWhite3002 <145886161+SamWhite3002@users.noreply.github.com>
2023-10-20 17:59:28 +1100

    Code for environment so far - 20/10/23

---
+++ Lab Assignment 2/AssessmentCode.m
@@ -0,0 +1,55 @@
+% Defining of figure and clear previous plots
+figure(1);
+clf;  %clear figure ensure it is all fresh for initialisation
+pause_simulation = false;
+
+
+
+%while(pause-simulation == false)
+
+    % Plot the robot and ensure subsequent plots go to the same figure
+    r1 = LinearUR3;
+    disp('LinearUR3 loaded')
+    %r2 = Gofa5;
+    %disp('Gofa5 loaded')
+    hold on;
+
+
+
+
+
+
+
+    BarEnvironment();
+    disp('Bar Background loaded')
+    disp('Fire Extinguisher loaded')
+    disp('Emergency Stop Button loaded')
+    Table();
+    disp('Table Loaded')
+    EnvironmentA2();
+    disp('Walls and Floors loaded')
+    BeerGlasses();
+    disp('Schooner glass placed')
+    PlaceSchooner();
+    BeerBottle();
+    disp('Beer bottle placed')
+
+    q = zeros(1,7);
+    r1.model.base = transl(-0.375,1.2,1.2) * trotx(pi/2) * troty(pi/2);
+    r1.model.animate(q);
+
+    % q1 = zeros(1,7);
+    % r2.model.base = transl(-0.375,1.2,1.2) * trotx(pi/2) * troty(pi/2);
+    % r2.model.animate(q1);
+
+
+
+
+
+    axis([-2 2 -2 2 -0 2]);  %Axis size
+    view(3);    % Sets the 3D view for the plot
+
+
+
+
+
