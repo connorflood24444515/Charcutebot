04411cea63b36276e935a43c5ee8737cef12c53e
drummels <149123744+drummels@users.noreply.github.com>
2023-10-29 12:19:46 +1100

    Add files via upload

---
+++ Lab Assignment 2/Lab Assignment 2/TrayMove/MoveTray copy.m
@@ -0,0 +1,44 @@
+function MoveTray()
+    
+    r1 = LinearUR3;
+
+    r1.model.base = transl(-0.375,1.2,1.2) * trotx(pi/2) * troty(pi/2);
+
+    q0 = [-0.8 11 74 0 213.45 300 0];
+    qf = [-0.01 8 74 0 213.45 300 0];
+
+    steps = 200;
+    qPath = jtraj(q0, qf, steps);
+
+    % Create a TrayObject and initialize its position
+    tray = ServiceTray(); % Pass the robot model
+
+    for i = 1:steps
+        r1.model.animate(qPath(i, :));
+        drawnow();
+
+        % Get the end effector's pose from the robot model
+        endEffectorPose = r1.model.fkine(qPath(i, :));
+
+        % Update the tray model's pose to match the end effector's pose
+        tray.model.base = endEffectorPose;
+        tray.model.animate(0); % Update the tray's position
+    end
+
+    pause(0);
+
+    % Reverse the motion
+    qPath = jtraj(qf, q0, steps);
+
+    for i = 1:steps
+        r1.model.animate(qPath(i, :));
+        drawnow();
+
+        endEffectorPose = r1.model.fkine(qPath(i, :));
+        tray.model.base = endEffectorPose;
+        tray.model.animate(0);
+    end
+
+    pause(0);
+
+end
